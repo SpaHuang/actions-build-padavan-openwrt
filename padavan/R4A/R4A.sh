55cd40fa0c6f1eddb31c50a488c15a225f823f2d
SpaHuang <375430227@qq.com>
2020-08-17 11:02:02 +0800

    Update R4A.sh

--- padavan/R4A/R4A.sh
+++ padavan/R4A/R4A.sh
@@ -1,15 +1,3 @@
 # 科学
 echo "CONFIG_FIRMWARE_INCLUDE_V2RAY=n" >> .config        # 集成v2ray执行文件（3.8M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
 echo "CONFIG_FIRMWARE_INCLUDE_TROJAN=n" >> .config       # 集成trojan执行文件(1.1M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
-
-# 广告
-echo "CONFIG_FIRMWARE_INCLUDE_KOOLPROXY=n" >> .config    # KP 广告过滤
-echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOME=y" >> .config  # ADGUARD 广告拦截
-
-# 代理
-echo "CONFIG_FIRMWARE_INCLUDE_KUMASOCKS=y" >> .config    # KUMA
-
-# 穿透
-echo "CONFIG_FIRMWARE_INCLUDE_FRPC=y" >> .config         # 内网穿透FRPC
-echo "CONFIG_FIRMWARE_INCLUDE_FRPS=y" >> .config         # 内网穿透FRPS
-echo "CONFIG_FIRMWARE_INCLUDE_ALIDDNS=y" >> .config      # 阿里DDNS
